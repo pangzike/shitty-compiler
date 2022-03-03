9df4b77bbcee1cb18f1cd8094987461ef609696b
Niko Yang <yangk59@126.com>
2022-03-03 17:39:10 +0800

    begin

---
+++ compiler/compiler/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  compiler
+//
+//  Created by 杨可 on 2022/3/3.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
